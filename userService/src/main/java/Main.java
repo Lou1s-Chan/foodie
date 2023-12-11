83a4fe53f379fbc1688bfc7cf3896948b575e54d
Minahhh666 <113988625+Minahhh666@users.noreply.github.com>
2023-12-11 15:40:27 +0000

    v0.0.7 working on restaurant connection

--- userService/src/main/java/Main.java
+++ userService/src/main/java/Main.java
@@ -12,6 +12,7 @@ import akka.actor.ActorSelection;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
 import ie.foodie.messages.CustomerOrderMessage;
+import ie.foodie.messages.RestaurantQueryMessage;
 import ie.foodie.messages.models.Customer;
 import ie.foodie.messages.models.Order;
 import ie.foodie.messages.models.Order.OrderDetail;
@@ -20,34 +21,34 @@ import service.UserActor;
 
 public class Main {
     public static void main(String[] args) {
-        // ask user to enter username and password
-        System.out.println("Welcome to Foodie!");
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter your username: ");
-        String username = scanner.nextLine();
-        System.out.print("Enter your password: ");
-        String password = scanner.nextLine();
-        scanner.close();
+        // // ask user to enter username and password
+        // System.out.println("Welcome to Foodie!");
+        // Scanner scanner = new Scanner(System.in);
+        // System.out.print("Enter your username: ");
+        // String username = scanner.nextLine();
+        // System.out.print("Enter your password: ");
+        // String password = scanner.nextLine();
+        // scanner.close();
 
-        // compare user input to database
-        String url = "jdbc:sqlite:userService/database/userdatabase.db";
-        try (Connection conn = DriverManager.getConnection(url)) {
-            if (conn != null) {
-                HashMap<String, String> nameAndPassword = getNameAndPasword(conn);
-                for (Map.Entry<String, String> entry : nameAndPassword.entrySet()) {
+        // // compare user input to database
+        // String url = "jdbc:sqlite:userService/database/userdatabase.db";
+        // try (Connection conn = DriverManager.getConnection(url)) {
+        // if (conn != null) {
+        // HashMap<String, String> nameAndPassword = getNameAndPasword(conn);
+        // for (Map.Entry<String, String> entry : nameAndPassword.entrySet()) {
 
-                    // login details are correct
-                    if (username.equals(entry.getKey()) && (password.equals(entry.getValue()))) {
-                        System.out.println("Login success!");
-                        // initiate the customer with all details from database
-                        Customer user = getUserDetails(conn, entry.getKey());
-                    }
-                }
-                conn.close();
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
+        // // login details are correct
+        // if (username.equals(entry.getKey()) && (password.equals(entry.getValue()))) {
+        // System.out.println("Login success!");
+        // // initiate the customer with all details from database
+        // Customer user = getUserDetails(conn, entry.getKey());
+        // }
+        // }
+        // conn.close();
+        // }
+        // } catch (SQLException e) {
+        // e.printStackTrace();
+        // }
 
         // creating the system and actor for USER
         ActorSystem system = ActorSystem.create("user-system");
@@ -57,19 +58,23 @@ public class Main {
         ActorSelection selection1 = system
                 .actorSelection("akka.tcp://restaurant-system@localhost:2551/user/restaurant-service");
         System.out.println("user make a query to restaurant system");
-        // selection1.tell(new RestaurantQueryMessage("query"), ref);
+        selection1.tell(new RestaurantQueryMessage(RestaurantQueryMessage.QueryType.RESTAURANT_LIST), ref);
 
-        // send message to ORDER
-        ActorSelection selection2 = system
-                .actorSelection("akka.tcp://order-system@localhost:2553/user/order-service");
-        System.out.println("user make an order to order system");
-        // V0.0.2 instantiate the restaurants object myself
-        Restaurant restaurant1 = new Restaurant(1, "123456789", "Dublin1");
-        OrderDetail[] orderDetail1 = { new OrderDetail(1, 18.88, 3), new OrderDetail(2, 28.88, 5) };
-        Restaurant restaurant2 = new Restaurant(2, "987654321", "Dublin2");
-        OrderDetail[] orderDetail2 = { new OrderDetail(3, 7.99, 2), new OrderDetail(4, 59.99, 4) };
-        Order[] order1 = { new Order(restaurant1, orderDetail1), new Order(restaurant2, orderDetail2) };
-        selection2.tell(new CustomerOrderMessage(new Customer(1, "Dublin1", "123456789"), order1), ref);
+        // // send message to ORDER
+        // ActorSelection selection2 = system
+        // .actorSelection("akka.tcp://order-system@localhost:2553/user/order-service");
+        // System.out.println("user make an order to order system");
+        // // V0.0.2 instantiate the restaurants object myself
+        // Restaurant restaurant1 = new Restaurant(1, "123456789", "Dublin1");
+        // OrderDetail[] orderDetail1 = { new OrderDetail(1, 18.88, 3), new
+        // OrderDetail(2, 28.88, 5) };
+        // Restaurant restaurant2 = new Restaurant(2, "987654321", "Dublin2");
+        // OrderDetail[] orderDetail2 = { new OrderDetail(3, 7.99, 2), new
+        // OrderDetail(4, 59.99, 4) };
+        // Order[] order1 = { new Order(restaurant1, orderDetail1), new
+        // Order(restaurant2, orderDetail2) };
+        // selection2.tell(new CustomerOrderMessage(new Customer(1, "Dublin1",
+        // "123456789"), order1), ref);
     }
 
     // get all username and password from database
