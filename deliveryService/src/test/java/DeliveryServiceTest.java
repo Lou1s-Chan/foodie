8cf8350ae078c233ff665e7202fd1b7d501eea94
Lou1s-Chan <tchen441@gmail.com>
2023-12-11 11:29:04 +0000

    Add test case for delivery service.

---
+++ deliveryService/src/test/java/DeliveryServiceTest.java
@@ -0,0 +1,45 @@
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.testkit.javadsl.TestKit;
+import ie.foodie.messages.OrderDeliveringMessage;
+import ie.foodie.messages.OrderDeliveryMessage;
+import ie.foodie.services.DeliveryService;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class DeliveryServiceTest {
+    static ActorSystem system;
+
+    @BeforeClass
+    public static void setup() {
+        system = ActorSystem.create();
+    }
+
+    @AfterClass
+    public static void teardown() {
+        TestKit.shutdownActorSystem(system);
+        system = null;
+    }
+
+    @Test
+    public void testDelivery() {
+        TestKit probe = new TestKit(system);
+        ActorRef deliveryService = system.actorOf(Props.create(DeliveryService.class));
+
+        int testOrderId = 999;
+        FakeOrder fakeOrder = new FakeOrder();
+        FakeCustomer fakeCustomer = new FakeCustomer();
+
+        OrderDeliveryMessage testDeliveryMessage = new OrderDeliveryMessage(testOrderId, fakeOrder, fakeCustomer);
+        deliveryService.tell(testDeliveryMessage, probe.getRef());
+
+        OrderDeliveringMessage response = probe.expectMsgClass(OrderDeliveringMessage.class);
+
+        assertEquals("DELIVERED", response.getStatus());
+        assertEquals("Order delivered", response.getMessage());
+        assertEquals(testOrderId, response.getOrderId());
+    }
+}
