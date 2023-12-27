04e02bdf4d47fa6c68c7e5d44cc0a4b2ef6124a2
Minahhh666 <113988625+Minahhh666@users.noreply.github.com>
2023-12-27 16:04:59 +0000

    v0.0.11 userDB with ID and cardnumber

--- userService/src/main/java/service/UserActor.java
+++ userService/src/main/java/service/UserActor.java
@@ -46,128 +46,127 @@ public class UserActor extends AbstractActor {
     private ActorSelection paymentServiceActor;
 
     public UserActor(ActorSystem system) {
-        this.paymentServiceActor = system.actorSelection("akka.tcp://payment-system@localhost:2555/user/payment-service");
+        this.paymentServiceActor = system
+                .actorSelection("akka.tcp://payment-system@localhost:2555/user/payment-service");
     }
 
     private static MongoClient mongoClient;
     private static MongoDatabase database;
     private static String dbURL = "mongodb+srv://foodie:ccOUvdosBLzDprGM@foodie.cli5iha.mongodb.net/?retryWrites=true&w=majority";
-    private static MongoCollection<Document> collection;    
+    private static MongoCollection<Document> collection;
 
     @Override
     public Receive createReceive() {
         return new ReceiveBuilder()
-            .match(Customer.class,
-                    msg -> {
-                        this.customerId = msg.getCustomerId();
-                        customerAddress = msg.getCustomerAddress();
-                        customerPhone = msg.getCustomerPhone();
-                    })
-            .match(OrderConfirmMessage.class, this::requestPayment)                    
-            .match(PaymentStatusMessage.class, msg -> {
-                // Handle the payment status message
-                System.out.println("Payment Status for Order ID " + msg.getOrderId() + ": " 
-                                    + msg.getStatus() + " - " + msg.getMessage());
-                // Additional logic based on the payment status
-            })
-            .match(RestaurantsResponse.class,
-                    msg -> {
-                        System.out.println("Received back Restaurant Response...");
-                        List<RestaurantData> restaurantList = msg.getRestaurants();
-                        for (RestaurantData restaurant : restaurantList) {
-                            System.out.println(restaurant.toString());
-                        }
-                        while (true) {
-                            System.out.print("Enter a restaurant id to check its menu: ");
+                .match(Customer.class,
+                        msg -> {
+                            this.customerId = msg.getCustomerId();
+                            customerAddress = msg.getCustomerAddress();
+                            customerPhone = msg.getCustomerPhone();
+                        })
+                .match(OrderConfirmMessage.class, this::requestPayment)
+                .match(PaymentStatusMessage.class, msg -> {
+                    // Handle the payment status message
+                    System.out.println("Payment Status for Order ID " + msg.getOrderId() + ": "
+                            + msg.getStatus() + " - " + msg.getMessage());
+                    // Additional logic based on the payment status
+                })
+                .match(RestaurantsResponse.class,
+                        msg -> {
+                            System.out.println("Received back Restaurant Response...");
+                            List<RestaurantData> restaurantList = msg.getRestaurants();
+                            for (RestaurantData restaurant : restaurantList) {
+                                System.out.println(restaurant.toString());
+                            }
+                            while (true) {
+                                System.out.print("Enter a restaurant id to check its menu: ");
 
-                            // Check if the next input is an integer
-                            if (scanner.hasNextInt()) {
-                                int selectedRestaurantId = scanner.nextInt();
-                                if ((selectedRestaurantId > 0) && (selectedRestaurantId < 31)) {
-                                    scanner.nextLine();
-                                    getSender().tell(
-                                            new RestaurantQueryMessage(
-                                                    RestaurantQueryMessage.QueryType.MENU_REQUEST,
-                                                    selectedRestaurantId),
-                                            getSelf());
-                                    for (RestaurantData restaurant : restaurantList) {
-                                        if (restaurant.getId() == selectedRestaurantId) {
-                                            restaurantId = restaurant.getId();
-                                            restaurantPhone = restaurant.getName();
-                                            restaurantAddress = restaurant.getAddress();
+                                // Check if the next input is an integer
+                                if (scanner.hasNextInt()) {
+                                    int selectedRestaurantId = scanner.nextInt();
+                                    if ((selectedRestaurantId > 0) && (selectedRestaurantId < 31)) {
+                                        scanner.nextLine();
+                                        getSender().tell(
+                                                new RestaurantQueryMessage(
+                                                        RestaurantQueryMessage.QueryType.MENU_REQUEST,
+                                                        selectedRestaurantId),
+                                                getSelf());
+                                        for (RestaurantData restaurant : restaurantList) {
+                                            if (restaurant.getId() == selectedRestaurantId) {
+                                                restaurantId = restaurant.getId();
+                                                restaurantPhone = restaurant.getName();
+                                                restaurantAddress = restaurant.getAddress();
+                                            }
+                                        }
+                                        System.out.println("Menu request sent!");
+                                        break; // Exit the loop if a valid integer is entered
+                                    } else {
+                                        System.out.println("Invalid input. Please enter a valid integer.");
+                                        scanner.nextLine(); // Consume the invalid input to avoid an infinite loop
+                                    }
+                                }
+                            }
+                        })
+                .match(MenuItemsResponse.class,
+                        msg -> {
+                            System.out.println("Received back menu Response...");
+                            ArrayList<MenuItemsResponse.MenuItemData> menuList = msg.getMenuItems();
+                            for (MenuItemsResponse.MenuItemData food : menuList) {
+                                System.out.println(food.toString());
+                            }
+
+                            while (true) {
+                                boolean orderHandled = false;
+                                System.out.print("Enter the Id of the food you want to order: ");
+                                int userInput = scanner.nextInt();
+                                scanner.nextLine();
+                                for (OrderDetail food : orderDetail) {
+                                    if (userInput == food.getFoodId()) {
+                                        food.setQuantity(food.getQuantity() + 1);
+                                        orderHandled = true;
+                                    }
+                                }
+                                if (!orderHandled) {
+                                    for (MenuItemsResponse.MenuItemData food : menuList) {
+                                        if (userInput == food.getItemId()) {
+                                            orderDetail.add(new OrderDetail(food.getItemId(), food.getPrice(), 1));
                                         }
                                     }
-                                    System.out.println("Menu request sent!");
-                                    break; // Exit the loop if a valid integer is entered
-                                } else {
-                                    System.out.println("Invalid input. Please enter a valid integer.");
-                                    scanner.nextLine(); // Consume the invalid input to avoid an infinite loop
+                                }
+                                System.out.println("Order added to your cart!");
+                                System.out.println("Current cart:");
+                                for (OrderDetail food : orderDetail) {
+                                    System.out.println("FoodID: " + food.getFoodId() + " Price: " + food.getPrice()
+                                            + " Quantity: " + food.getQuantity());
+                                }
+                                System.out.println(
+                                        "Enter 'Yes' to continue ordering, or 'No' to finish and send the order.");
+                                if (scanner.nextLine().toLowerCase().equals("no")) {
+                                    break;
                                 }
                             }
-                        }
-                    })
-            .match(MenuItemsResponse.class,
-                    msg -> {
-                        System.out.println("Received back menu Response...");
-                        ArrayList<MenuItemsResponse.MenuItemData> menuList = msg.getMenuItems();
-                        for (MenuItemsResponse.MenuItemData food : menuList) {
-                            System.out.println(food.toString());
-                        }
+                            ActorSystem system = getContext().getSystem();
+                            ActorSelection orderSystem = system
+                                    .actorSelection("akka.tcp://order-system@localhost:2553/user/order-service");
+                            System.out.println("user make an order to order system");
 
-                        while (true) {
-                            boolean orderHandled = false;
-                            System.out.print("Enter the Id of the food you want to order: ");
-                            int userInput = scanner.nextInt();
-                            scanner.nextLine();
-                            for (OrderDetail food : orderDetail) {
-                                if (userInput == food.getFoodId()) {
-                                    food.setQuantity(food.getQuantity() + 1);
-                                    orderHandled = true;
-                                }
-                            }
-                            if (!orderHandled) {
-                                for (MenuItemsResponse.MenuItemData food : menuList) {
-                                    if (userInput == food.getItemId()) {
-                                        orderDetail.add(new OrderDetail(food.getItemId(), food.getPrice(), 1));
-                                    }
-                                }
-                            }
-                            System.out.println("Order added to your cart!");
-                            System.out.println("Current cart:");
-                            for (OrderDetail food : orderDetail) {
-                                System.out.println("FoodID: " + food.getFoodId() + " Price: " + food.getPrice()
-                                        + " Quantity: " + food.getQuantity());
-                            }
-                            System.out.println(
-                                    "Enter 'Yes' to continue ordering, or 'No' to finish and send the order.");
-                            if (scanner.nextLine().toLowerCase().equals("no")) {
-                                break;
-                            }
-                        }
-                        ActorSystem system = getContext().getSystem();
-                        ActorSelection orderSystem = system
-                                .actorSelection("akka.tcp://order-system@localhost:2553/user/order-service");
-                        System.out.println("user make an order to order system");
-
-                        Restaurant restaurant = new Restaurant(restaurantId, restaurantPhone,
-                                restaurantAddress);
-                        // OrderDetail[] orderDetail = { new OrderDetail(1, 18.88, 3), new
-                        // OrderDetail(2, 28.88, 5) };
-                        ArrayList<Order> order = new ArrayList<>();
-                        order.add(new Order(restaurant, orderDetail));
-                        orderSystem.tell(new CustomerOrderMessage(
-                                new Customer(customerId, customerAddress, customerPhone), order), getSelf());
-                    })
+                            Restaurant restaurant = new Restaurant(restaurantId, restaurantPhone,
+                                    restaurantAddress);
+                            ArrayList<Order> order = new ArrayList<>();
+                            order.add(new Order(restaurant, orderDetail));
+                            orderSystem.tell(new CustomerOrderMessage(
+                                    new Customer(customerId, customerAddress, customerPhone), order), getSelf());
+                        })
                 .build();
     }
 
-    private void requestPayment(OrderConfirmMessage msg){
+    private void requestPayment(OrderConfirmMessage msg) {
         double amountToPay = msg.getTotalPrice();
         int orderId = msg.getOrderId();
 
         System.out.print("Enter payment method (Card/Cash): ");
         String paymentMethod = scanner.nextLine().trim();
-    
+
         // Validate input and default to Card if invalid
         if (!paymentMethod.equalsIgnoreCase("Card") && !paymentMethod.equalsIgnoreCase("Cash")) {
             System.out.println("Invalid input. Defaulting to Card.");
@@ -179,8 +178,8 @@ public class UserActor extends AbstractActor {
             System.out.println("Checking saved card for Customer " + customerId + 1);
             String encryptedCardNumber = getEncryptedCardNumber(this.customerId);
 
-            String cardNumber;  
-            
+            String cardNumber;
+
             try (Scanner scanner = new Scanner(System.in)) {
                 if (encryptedCardNumber != null) {
                     // Decrypt the card number for display purposes
@@ -204,9 +203,10 @@ public class UserActor extends AbstractActor {
                     updateUserCardNumberInDatabase(this.customerId, encryptedCardNumber);
                 }
             }
-        }                                                   
-    
-        OrderPaymentMessage paymentMessage = new OrderPaymentMessage(orderId, this.customerId, amountToPay, paymentMethod);
+        }
+
+        OrderPaymentMessage paymentMessage = new OrderPaymentMessage(orderId, this.customerId, amountToPay,
+                paymentMethod);
         paymentServiceActor.tell(paymentMessage, getSelf());
     }
 
@@ -230,12 +230,12 @@ public class UserActor extends AbstractActor {
             System.out.println("No filtered card number found" + 5);
             return null; // No card number found
         }
-        
+
     }
 
     private String encryptCardNumber(String cardNumber) {
-    // Implement encryption logic here
-    // Example: use AES or similar encryption algorithm
+        // Implement encryption logic here
+        // Example: use AES or similar encryption algorithm
         String encryptedCardNumber = cardNumber;
         return encryptedCardNumber; // Return the encrypted card number
     }
