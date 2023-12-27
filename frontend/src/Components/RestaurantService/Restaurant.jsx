569f58badf6234a45685bae938c9816f9bee1e6b
Itgel Ganbold <itgel98@gmail.com>
2023-12-27 21:42:58 +0000

    Added reconnect method to front-end.

--- frontend/src/Components/RestaurantService/Restaurant.jsx
+++ frontend/src/Components/RestaurantService/Restaurant.jsx
@@ -1,100 +1,37 @@
-// import React, { useEffect, useState } from "react";
-// import SockJS from "sockjs-client";
-// import Stomp from "stompjs";
-
-// const Restaurant = () => {
-//   const [orderMessage, setOrderMessage] = useState("");
-//   let stompClient = null;
-
-//   const reconnect = () => {
-//     setTimeout(() => {
-//       connectToWebSocket();
-//     }, 5000); // Reconnect after 5 seconds
-//   };
-
-//   const connectToWebSocket = () => {
-//     const socket = new SockJS("http://localhost:8080/ws");
-//     stompClient = Stomp.over(socket);
-
-//     stompClient.connect(
-//       {},
-//       function (frame) {
-//         stompClient.subscribe(
-//           "/topic/restaurantService",
-//           function (orderMessage) {
-//             setOrderMessage(orderMessage.body);
-//             console.log(orderMessage.body);
-//           }
-//         );
-//       },
-//       function (error) {
-//         console.error("Connection error: ", error);
-//         reconnect();
-//       }
-//     );
-
-//     socket.onclose = function () {
-//       console.log("WebSocket connection closed");
-//       reconnect();
-//     };
-//   };
-
-//   useEffect(() => {
-//     connectToWebSocket();
-
-//     return () => {
-//       if (stompClient && stompClient.connected) {
-//         stompClient.disconnect();
-//       }
-//     };
-//   }, []);
-
-//   useEffect(() => {
-//     console.log({ orderMessage });
-//   }, [orderMessage]);
-
-//   return (
-//     <div>
-//       <h1>OrderMessages</h1>
-//       <div>{orderMessage}</div>
-//     </div>
-//   );
-// };
-
-// export default Restaurant;
-
 import React, { useEffect, useState } from "react";
 
 const Restaurant = () => {
   const [orderMessage, setOrderMessage] = useState("");
 
   useEffect(() => {
-    // Create a new EventSource that connects to your SSE endpoint
-    const eventSource = new EventSource("http://localhost:8080/stream");
+    let eventSource;
+    const connectToSSE = () => {
+      eventSource = new EventSource("http://localhost:8080/stream");
+      eventSource.onmessage = (event) => {
+        const data = JSON.parse(event.data);
+        console.log("Received message:", data);
+        setOrderMessage(data);
+      };
 
-    // Listen for messages
-    eventSource.onmessage = (event) => {
-      const data = JSON.parse(event.data);
-      console.log("Received message:", data);
-      setOrderMessage(data);
+      eventSource.onerror = (error) => {
+        console.error("EventSource failed:", error);
+        eventSource.close();
+        setTimeout(connectToSSE, 5000); // Try to reconnect after 5 seconds
+      };
     };
 
-    // Handle any errors
-    eventSource.onerror = (error) => {
-      console.error("EventSource failed:", error);
-      eventSource.close();
-    };
+    connectToSSE();
 
-    // Clean up on component unmount
     return () => {
-      eventSource.close();
+      if (eventSource) {
+        eventSource.close();
+      }
     };
   }, []);
 
   return (
     <div>
-      <h1>Order Messages</h1>
-      <div>{orderMessage}</div>
+      <div>{JSON.stringify(orderMessage)}</div>
     </div>
   );
 };
